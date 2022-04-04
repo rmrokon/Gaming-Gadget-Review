3438ac202c756f2c6061c7f7ae0793d37dbfcfc0
rmrokon <mdrokon7773@gmail.com>
2022-04-05 00:23:26 +0600

    review items responsiveness done

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -8,24 +8,24 @@ const Home = () => {
     const reviews = useReview();
     return (
         <div>
-            <div className='flex flex-row justify-around'>
-                <div className='basis-1/2 mt-20'>
+            <div className='flex flex-row justify-around items-center p-5'>
+                <div className='md:basis-1/2 mt-20'>
                     <h3 className='text-3xl font-bold mb-5'>Xbox Core Wireless Controller – Carbon Black</h3>
                     <p className='text-lg'>Experience the latest design of Xbox. Completely wireless controller. Stay on target with the hybrid D-pad. Connect using the USB-C port for direct plug and play to console or PC. Support for AA batteries is included on the rear.</p>
                     <button className='mt-5 bg-orange-100 p-5 rounded-md text-red-600  text-lg font-bold'>Watch Gameplay</button>
                 </div>
                 <div className=''>
-                    <img src={Image} alt="" />
+                    <img className='' src={Image} alt="" />
                 </div>
             </div>
             <div>
                 <h3 className='text-center text-3xl font-bold'>Customer Review ({reviews.length})</h3>
-                <div className='flex p-10'>
+                <div className='grid md:grid-cols-3 p-3 gap-5 sm:w-1/4 md:w-full'>
                     {
                         reviews.slice(0, 3).map(review => <SingleReview key={review._id} {...review}></SingleReview>)
                     }
                 </div>
-                <div className='text-center mb-10'>
+                <div className='text-center my-10'>
                     <Link className='bg-orange-100 text-red-600 font-bold text-lg p-5 rounded-lg' to={'reviews'}>See All Customer Reviews</Link>
                 </div>
             </div>
--- src/Components/SingleReview/SingleReview.js
+++ src/Components/SingleReview/SingleReview.js
@@ -3,12 +3,12 @@ import { StarIcon } from '@heroicons/react/solid'
 
 const SingleReview = ({ picture, name, rating, review }) => {
     return (
-        <div className='border-2 rounded-lg p-5 w-1/2 mr-5'>
+        <div className='border-2 rounded-lg p-5 sm:w-1/4  md:w-full'>
             <div className='flex items-center'>
                 <img className='rounded-lg' src={picture} alt="" />
                 <p className='ml-5 text-xl font-bold'>{name}</p>
             </div>
-            <p className='flex  mt-3 align-middle items-center font-bold text-lg'><span className='font-bold mr-3'>Rating:</span> {rating}<StarIcon className='w-5 h-5 text-yellow-500'></StarIcon></p>
+            <p className='flex mt-3 align-middle items-center font-bold text-lg'><span className='font-bold mr-3'>Rating:</span> {rating}<StarIcon className='w-5 h-5 text-yellow-500'></StarIcon></p>
             <p className='my-3 text-lg'>{review}</p>
         </div>
     );
